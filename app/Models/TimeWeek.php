2211ea37398b2d2f768d6a9655ace22ba3484e54
Ivars Šaudinis <ivars.saudinis@liepu.lv>
2021-10-04 23:00:12 +0300

    Laika tabulu modeļi

---
+++ app/Models/TimeWeek.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TimeWeek extends Model
+{
+    use HasFactory;
+
+    protected $table = "timetable_week";
+
+}
---
+++ app/Models/TimeYear.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TimeYear extends Model
+{
+    use HasFactory;
+
+    protected $table = "timetable_year";
+}
