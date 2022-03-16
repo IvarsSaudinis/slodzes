2238703afa47839263eb2ee50327a8531f6f3990
Ivars Šaudinis <ivars.saudinis@liepu.lv>
2022-03-16 10:47:24 +0200

    pogu nosaukumu maiņa

--- app/Http/Livewire/UserTable.php
+++ app/Http/Livewire/UserTable.php
@@ -120,13 +120,13 @@ final class UserTable extends PowerGridComponent
     {
         return [
             Button::add('create-user')
-                ->caption('Pievienot lietotāju/mācībspēku')
+                ->caption('Pievienot lietotāju')
                 ->class('inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md text-sm mx-2 text-white tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150 ml-0')
                 ->route('users.create', []),
 
             Button::add('binding-modules-users')
                 ->caption(__('Moduļu sasaiste'))
-                ->class('inline-flex items-center px-4 py-2 bg-gray-300 border border-transparent rounded-md text-sm mx-2 text-white tracking-widest hover:bg-gray-700 active:bg-gray-300 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150 ml-0')
+                ->class('inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500')
                 // https://github.com/wire-elements/modal
                 ->emit('openModalModules', []),
                 // 'pg:eventRefresh-users'  => '$refresh',
