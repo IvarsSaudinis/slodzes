9b4e64fe2bb95abd463bb716b59880d5d1d210d9
ivars <ivars@ivars.lv>
2022-06-02 22:54:55 +0300

    to nākošai dienai

--- resources/views/plans/form.blade.php
+++ resources/views/plans/form.blade.php
@@ -1,12 +1,18 @@
 <x-app-layout>
     <x-slot name="header">
-        {{ __('Mācību plāni') }}
+        {{ __('Mācību plāns') }}
     </x-slot>
 
     <div class="py-2">
         <div class=" mx-auto sm:px-6 lg:px-8  ">
             <x-message>{{ session('message') }}</x-message>
 
+           <strong>todo:</strong>
+            <ul>
+                <li>Plāna datu izvade (moduļu saraksts)</li>
+                <li>Iespēja modulim piesaistīt pasniedzēju</li>
+                <li>Iepsēja nodefinēt stundu skaitu nedeļā</li>
+            </ul>
         </div>
     </div>
 </x-app-layout>
