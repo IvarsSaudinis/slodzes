4b5898d0c897a57a27922f59d9a7a9b59ee4f8a9
ivars <ivars@ivars.lv>
2022-07-21 17:11:08 +0300

    rāda aktīvo mācību gadu

--- app/Http/Controllers/TimeSettingsController.php
+++ app/Http/Controllers/TimeSettingsController.php
@@ -5,14 +5,17 @@ namespace App\Http\Controllers;
 use App\Models\EduWeek;
 use App\Models\EduYear;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 
 class TimeSettingsController extends Controller
 {
     public function index()
     {
-        $weeks = EduWeek::where('edu_year_id', 1)->get();
+        $active_year = Session::get('edu_year', 1)->id;
 
-        $year = EduYear::find(1);
+        $weeks = EduWeek::where('edu_year_id', $active_year)->get();
+
+        $year = EduYear::find($active_year);
 
         return view('time-settings.index', compact('weeks', 'year'));
     }
